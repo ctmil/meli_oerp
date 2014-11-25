489d036e62c4c1c28ac334277bfbd4fd34451d36
morrillo <gustavo.orrillo@moldeo.coop>
2014-11-25 16:09:27 -0200

    First commit

---
+++ __init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import company
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ __openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#       Pere Ramon Erro Mas <pereerro@tecnoba.com> All Rights Reserved.
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'MercadoLibre Connector',
+    'version': '0.1',
+    'author': 'Moldeo Interactive',
+    'website': 'http://business.moldeo.coop',
+    "category": "Sales",
+    "depends": ['base', 'product'],
+    'data': [
+	'company_view.xml'
+    ],
+    'demo_xml': [],
+    'active': False,
+    'installable': True,
+    'application': True,
+}
