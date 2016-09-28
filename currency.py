0db2be580f17091d4634d6bafd06578c4e3dda5c
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2016-09-28 15:12:37 -0300

    recheck

--- currency.py
+++ currency.py
@@ -4,6 +4,12 @@ from openerp import models, fields, api, _
 class res_currency(models.Model):
     _inherit = 'res.currency'
 
+    @api.one
+    @api.onchange('rate') # if these fields are changed, call method
+    def check_change_rate(self):
+        import pdb;pdb.set_trace();
+        self.update_prices()
+
     @api.model
     def update_prices(self):
         # Moneda en USD
@@ -14,9 +20,5 @@ class res_currency(models.Model):
             if product.list_price > 0 and currency_usd.rate>0:
                    product.meli_price = product.list_price / currency_usd.rate
 
-    @api.onchange('rate') # if these fields are changed, call method
-    def check_change_rate(self):
-        import pdb;pdb.set_trace();
-        self.update_prices()
 
 res_currency()
