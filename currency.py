7b1bc6e8abf8cdbbe77bba7eecdb68a8023d3da6
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2016-09-28 16:42:52 -0300

    Cleaning

--- currency.py
+++ currency.py
@@ -1,44 +1,21 @@
 
 from openerp import models, fields, api, _
 
-class res_currency(models.Model):
-    _inherit = 'res.currency'
-
-    @api.one
-    @api.onchange('rate') # if these fields are changed, call method
-    def check_change_rate(self):
-        import pdb;pdb.set_trace();
-        self.update_prices()
-
-    @api.model
-    def update_prices(self):
-        # Moneda en USD
-        currency_obj = self.pool.get('res.currency')
-        currency_usd = currency_obj.search(['name','=','USD'])
-        products = self.env['product.product'].search([])
-        for product in products:
-            if product.list_price > 0 and currency_usd.rate>0:
-                   product.meli_price = product.list_price / currency_usd.rate
-
-
-res_currency()
-
-
 class res_currency_rate(models.Model):
     _inherit = 'res.currency.rate'
 
     @api.one
     @api.onchange('rate') # if these fields are changed, call method
     def check_change_rate(self):
-        import pdb;pdb.set_trace();
         self.update_prices()
 
     @api.model
     def update_prices(self):
         products = self.env['product.product'].search([])
+        import pdb;pdb.set_trace();
         for product in products:
             if self.rate>0:
-                   product.meli_price = str(product.lst_price / self.rate)
+                product.meli_price = str(product.lst_price / self.rate)
 
 
 res_currency_rate()
