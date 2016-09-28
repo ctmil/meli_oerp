049ab32a1348018ccadab691fc48609f66c8f0cf
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2016-09-28 15:18:22 -0300

    recheck

--- currency.py
+++ currency.py
@@ -22,3 +22,27 @@ class res_currency(models.Model):
 
 
 res_currency()
+
+
+class res_currency_rate(models.Model):
+    _inherit = 'res.currency.rate'
+
+    @api.one
+    @api.onchange('rate') # if these fields are changed, call method
+    def check_change_rate(self):
+        import pdb;pdb.set_trace();
+        self.update_prices()
+
+    @api.model
+    def update_prices(self):
+        # Moneda en USD
+        currency_obj = self.pool.get('res.currency')
+        currency_usd = currency_obj.search(['name','=','USD'])
+        products = self.env['product.product'].search([])
+        for product in products:
+            if product.list_price > 0 and currency_usd.rate>0:
+                   product.meli_price = product.list_price / currency_usd.rate
+
+
+res_currency_rate()
+
