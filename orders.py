c571ea7a4cddc9d82b2fcbca350d06b000c646a9
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2016-10-27 10:29:52 -0300

    Fixing sale.order.line calls

--- orders.py
+++ orders.py
@@ -314,8 +314,8 @@ class mercadolibre_orders(osv.osv):
                     'company_id': company.id,
                     'order_id': sorder.id,
                     'meli_order_item_id': Item['item']['id'],
-                    'price_unit': product_related_obj.price,
-                    'price_total': product_related_obj.price * float(Item['quantity']),
+                    'price_unit': float(Item['unit_price']),
+                    'price_total': float(Item['unit_price']) * float(Item['quantity']),
                     'product_id': product_related_obj.id,
                     'product_uom_qty': Item['quantity'],
                 }
