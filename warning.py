b48dfae39fa5c5f954a30c0c709ab8014bc7dc4f
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2017-05-29 16:49:42 -0300

    from openerp to from odoo... osv_memory

--- warning.py
+++ warning.py
@@ -1,4 +1,5 @@
-from odoo import fields, osv, osv_memory, _
+from odoo import fields, osv, _
+from osv import osv_memory
 #from openerp.tools.translate import _
 
 #CHANGE WARNING_MODULE with your module name
