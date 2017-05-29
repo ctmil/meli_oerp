b2db174ddb1dd12ad986457e686facaabde55de3
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2017-05-29 18:34:07 -0300

    fixing indentations

--- questions.py
+++ questions.py
@@ -33,16 +33,15 @@ class mercadolibre_questions(models.Model):
 	_description = "Preguntas en MercadoLibre"
 
 	posting_id = fields.Many2one("mercadolibre.posting","Posting");
-    question_id = fields.Char('Question Id');
-    date_created = fields.Date('Creation date');
-    item_id = fields.Char(string="Item ID",size=255);
-    seller_id = fields.Char(string="Seller ID",size=255);
-    text = fields.Text("Question Text");
-    status = fields.Selection( [("UNANSWERED","Question is not answered yet."),("ANSWERED","Question was answered."),("CLOSED_UNANSWERED","The item is closed and the question was never answered."),("UNDER_REVIEW","The item is under review and the question too.")], string='Question Status');
-    answer_date_created = fields.Date('Answer creation date');
-    answer_status = fields.Selection( [("ACTIVE","Active"),("DISABLED","Disabled")], string='Answer Status');
-    answer_text = fields.Text("Answer Text");
-
-
+	question_id = fields.Char('Question Id');
+	date_created = fields.Date('Creation date');
+	item_id = fields.Char(string="Item ID",size=255);
+	seller_id = fields.Char(string="Seller ID",size=255);
+	text = fields.Text("Question Text");
+	status = fields.Selection( [("UNANSWERED","Question is not answered yet."),("ANSWERED","Question was answered."),("CLOSED_UNANSWERED","The item is closed and the question was never answered."),("UNDER_REVIEW","The item is under review and the question too.")], string='Question Status');
+	answer_date_created = fields.Date('Answer creation date');
+	answer_status = fields.Selection( [("ACTIVE","Active"),("DISABLED","Disabled")], string='Answer Status');
+	answer_text = fields.Text("Answer Text");
+	
 
 mercadolibre_questions()
