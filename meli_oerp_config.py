0ef4eae335caa0ea3dd2777387c3d8329191cd18
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2017-09-04 13:25:39 -0300

    Fixing meli_oerp_config

--- meli_oerp_config.py
+++ meli_oerp_config.py
@@ -1,6 +1,6 @@
 #meli_oerp_config
 
-REDIRECT_URI = 'https://odoo.fabriciocosta.com.ar/meli_login'
+REDIRECT_URI = 'https://www.mymbikes.com.ar/meli_login'
 ACCESS_TOKEN = ''
 REFRESH_TOKEN = ''
 CLIENT_ID = ''
