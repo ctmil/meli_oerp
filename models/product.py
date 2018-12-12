10668bcfb4f0eb25108047b04704fc6d72399238
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2018-12-12 17:48:30 -0300

    Optimizing image

--- models/product.py
+++ models/product.py
@@ -341,8 +341,11 @@ class product_product(models.Model):
                     pimage = self.env["product.image"].search([('meli_imagen_id','=',pic["id"]),('product_tmpl_id','=',product_template.id)])
                     #_logger.info(pimage)
                     if (pimage and pimage.image):
-                        bin_diff = len(image_base64) - len(pimage.image)
-                        _logger.info("Image:"+str(len(pimage.image))+" vs ImageB64:"+str(meli_imagen_bytes)+" diff:"+str(bin_diff) )
+                        bin_diff = len(image_base64) - len(pimage.image
+                        imagebin = base64.b64decode( pimage.image )
+                        _logger.info("ImageBinDecoded:"+str(len(imagebin)))
+                        _logger.info("Image:"+str(len(pimage.image))+" vs URLImageB64:"+str(meli_imagen_bytes)+" diff:"+str(bin_diff) )
+                        _logger.info("URLImageBin:"+str(len(image)))
                         bin_updating = (abs(bin_diff)>5000)
 
                 if (pimage==False or len(pimage)==0):
