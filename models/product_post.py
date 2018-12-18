d544fd19767ff4e4e7edaab74bf27e3b1c028acd
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2018-12-18 10:05:15 -0300

    Adding update product template wizard button

--- models/product_post.py
+++ models/product_post.py
@@ -107,6 +107,62 @@ class product_template_post(models.TransientModel):
 
 product_template_post()
 
+class product_template_update(models.TransientModel):
+    _name = "mercadolibre.product.template.update"
+    _description = "Wizard de Product Template Update en MercadoLibre"
+
+    type = fields.Selection([('post','Alta'),('put','Editado'),('delete','Borrado')], string='Tipo de operación' );
+    posting_date = fields.Date('Fecha del posting');
+	    #'company_id': fields.many2one('res.company',string='Company'),
+	    #'mercadolibre_state': fields.related( 'res.company', 'mercadolibre_state', string="State" )
+
+
+    def pretty_json( self, data ):
+        return json.dumps( data, sort_keys=False, indent=4 )
+
+    def product_template_update(self, context):
+        #pdb.set_trace()
+        company = self.env.user.company_id
+        product_ids = context['active_ids']
+        product_obj = self.env['product.template']
+
+        #user_obj = self.pool.get('res.users').browse(cr, uid, uid)
+        #user_obj.company_id.meli_login()
+        #company = user_obj.company_id
+        warningobj = self.env['warning']
+
+        #company = self.pool.get('res.company').browse(cr,uid,1)
+
+        REDIRECT_URI = company.mercadolibre_redirect_uri
+        CLIENT_ID = company.mercadolibre_client_id
+        CLIENT_SECRET = company.mercadolibre_secret_key
+        ACCESS_TOKEN = company.mercadolibre_access_token
+        REFRESH_TOKEN = company.mercadolibre_refresh_token
+
+
+        meli = Meli(client_id=CLIENT_ID,client_secret=CLIENT_SECRET, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
+
+        if ACCESS_TOKEN=='' or ACCESS_TOKEN==False:
+            meli = Meli(client_id=CLIENT_ID,client_secret=CLIENT_SECRET)
+            url_login_meli = meli.auth_url(redirect_URI=REDIRECT_URI)
+            return {
+	            "type": "ir.actions.act_url",
+	            "url": url_login_meli,
+	            "target": "new",
+            }
+        res = {}
+        for product_id in product_ids:
+            product = product_obj.browse(product_id)
+            if (product):
+                if (product.meli_pub):
+                    res = product.product_template_update()
+
+            if 'name' in res:
+                return res
+
+        return res
+
+product_template_update()
 
 class product_post(models.TransientModel):
     _name = "mercadolibre.product.post"
