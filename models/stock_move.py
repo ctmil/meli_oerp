a607e38ee7f46def02b2ae90bd75e48eae30758b
fabricio costa <fabricio.costa@moldeointeractive.com.ar>
2018-12-21 17:38:21 -0300

    Fix post stock and meli_id_variation

--- models/stock_move.py
+++ models/stock_move.py
@@ -14,42 +14,44 @@ class StockMove(models.Model):
 
     @api.multi
     def action_done(self):
+
         #import pdb; pdb.set_trace()
-        _logger.info("StockMove action_done")
-#        _logger.info(self)
-        _logger.info("Before: virtual av:" + str(self.product_id.virtual_available))
-        res = super(StockMove, self).action_done()
-        _logger.info("After: virtual av:" + str(self.product_id.virtual_available))
+        for mov in self:
+            _logger.info("StockMove action_done")
+            #_logger.info(self)
+            _logger.info("Before: virtual av:" + str(mov.product_id.virtual_available))
+            res = super(StockMove, mov).action_done()
+            _logger.info("After: virtual av:" + str(mov.product_id.virtual_available))
 
 
-        if self.product_id:
-            bomlines = self.env['mrp.bom.line'].search([('product_id','=',self.product_id.id)])
-            if (bomlines):
-                for bomline in bomlines:
-                    #_logger.info("bomline tpl: "+bomline.bom_id.product_tmpl_id.name)
-                    #_logger.info("bomline var: "+bomline.bom_id.product_id.name)
-                    #_logger.info("bomline var code: "+bomline.bom_id.product_id.default_code)
-                    _logger.info("Clone stock: " + str(bomline.bom_id.product_id.virtual_available))
-                    if (bomline.bom_id.product_id.virtual_available !=self.product_id.virtual_available):
-                        _logger.info("Trigger stock equivalence function:")
-                        movs = self.env['stock.move']
-                        qty = self.ordered_qty
-                        qtydiff = bomline.bom_id.product_id.virtual_available - self.product_id.virtual_available
-                        if (qtydiff>qty):
-                            qty = qtydiff
-                        movfields = {
-                            "name": self.name+str(' (clone)'),
-                            "product_id": bomline.bom_id.product_id.id,
-                            "location_id": self.location_id.id,
-                            "location_dest_id": self.location_dest_id.id,
-                            "procure_method": self.procure_method,
-                            "product_uom_qty": qty,
-                            #"ordered_qty": qty,
-                            "product_uom": self.product_uom.id
-                        }
-                        _logger.info(movfields)
-                        sm = movs.create(movfields)
-                        if (sm):
-                            sm.action_done()
+            if mov.product_id:
+                bomlines = self.env['mrp.bom.line'].search([('product_id','=',mov.product_id.id)])
+                if (bomlines):
+                    for bomline in bomlines:
+                        #_logger.info("bomline tpl: "+bomline.bom_id.product_tmpl_id.name)
+                        #_logger.info("bomline var: "+bomline.bom_id.product_id.name)
+                        #_logger.info("bomline var code: "+bomline.bom_id.product_id.default_code)
+                        _logger.info("Clone stock: " + str(bomline.bom_id.product_id.virtual_available))
+                        if (bomline.bom_id.product_id.virtual_available !=mov.product_id.virtual_available):
+                            _logger.info("Trigger stock equivalence function:")
+                            movs = self.env['stock.move']
+                            qty = mov.ordered_qty
+                            qtydiff = bomline.bom_id.product_id.virtual_available - mov.product_id.virtual_available
+                            if (qtydiff>qty):
+                                qty = qtydiff
+                            movfields = {
+                                "name": mov.name+str(' (clone)'),
+                                "product_id": bomline.bom_id.product_id.id,
+                                "location_id": mov.location_id.id,
+                                "location_dest_id": mov.location_dest_id.id,
+                                "procure_method": mov.procure_method,
+                                "product_uom_qty": qty,
+                                #"ordered_qty": qty,
+                                "product_uom": mov.product_uom.id
+                            }
+                            _logger.info(movfields)
+                            sm = movs.create(movfields)
+                            if (sm):
+                                sm.action_done()
 
         return True
