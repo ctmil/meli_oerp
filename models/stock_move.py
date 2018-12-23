b4e118e8b366574896aafaecbe3fc0a22c14852e
fabricio costa <fabricio.costa@moldeointeractive.com.ar>
2018-12-23 17:22:24 -0300

    Fix auto post_stock after stock move

--- models/stock_move.py
+++ models/stock_move.py
@@ -16,6 +16,7 @@ class StockMove(models.Model):
     def action_done(self):
 
         #import pdb; pdb.set_trace()
+        company = self.env.user.company_id
         for mov in self:
             _logger.info("StockMove action_done")
             #_logger.info(self)
@@ -28,9 +29,6 @@ class StockMove(models.Model):
                 bomlines = self.env['mrp.bom.line'].search([('product_id','=',mov.product_id.id)])
                 if (bomlines):
                     for bomline in bomlines:
-                        #_logger.info("bomline tpl: "+bomline.bom_id.product_tmpl_id.name)
-                        #_logger.info("bomline var: "+bomline.bom_id.product_id.name)
-                        #_logger.info("bomline var code: "+bomline.bom_id.product_id.default_code)
                         _logger.info("Clone stock: " + str(bomline.bom_id.product_id.virtual_available))
                         if (bomline.bom_id.product_id.virtual_available !=mov.product_id.virtual_available):
                             _logger.info("Trigger stock equivalence function:")
@@ -53,5 +51,8 @@ class StockMove(models.Model):
                             sm = movs.create(movfields)
                             if (sm):
                                 sm.action_done()
+                if (company.mercadolibre_cron_post_update_stock):
+                    if (mov.product_id.meli_id and mov.product_id.meli_pub):
+                        mov.product_id.product_post_stock()
 
         return True
