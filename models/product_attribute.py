129f2b8ca9fc85042e87cf0f2c2f8adff648abfb
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2019-01-22 01:26:21 -0300

    Adding default attribute

--- models/product_attribute.py
+++ models/product_attribute.py
@@ -9,5 +9,5 @@ class ProductAttribute(models.Model):
     _inherit = 'product.attribute'
 
     #meli_id = fields.Char(u'Id Attribute ML')
-    meli_default_id_attribute = fields.Many2many('mercadolibre.category.attribute',string="ML Attribute default")
+    meli_default_id_attribute = fields.Many2one('mercadolibre.category.attribute',string="ML Attribute default")
     meli_id_attributes = fields.Many2many('mercadolibre.category.attribute',string="ML Attributes")
