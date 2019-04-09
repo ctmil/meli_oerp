69f00cda9690a6667650c99bbfd1616f10173efb
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2019-04-09 08:31:25 -0300

    Fixing custom attributes non capitalized

--- models/product.py
+++ models/product.py
@@ -690,7 +690,7 @@ class product_product(models.Model):
                                 attribute_duplicates = self.env['product.attribute'].search([('name','=',attcomb['name']),('meli_default_id_attribute','=',False)])
                                 if (len(attribute_duplicates)>=1):
                                     #archive
-                                    _logger.info("attribute_duplicates:",attdup.name)
+                                    _logger.info("attribute_duplicates:",len(attribute_duplicates))
                                     for attdup in attribute_duplicates:
                                         _logger.info("duplicate:",attdup.name,attdup.id)
                                         #attdup.unlink()
