ebf6184cb23a745b1e3623c0b67cc893c3d03521
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2020-05-27 18:50:22 +0200

    Fix questions strings

--- models/posting.py
+++ models/posting.py
@@ -165,12 +165,12 @@ class mercadolibre_posting(models.Model):
                     'date_created': Question['date_created'],
                     'item_id': Question['item_id'],
                     'seller_id': Question['seller_id'],
-                    'text': str(Question['text'].encode("utf-8")),
+                    'text': Question['text'].encode("utf-8"),
                     'status': Question['status'],
                 }
 
                 if (question_answer):
-                    question_fields['answer_text'] = str(question_answer['text'].encode("utf-8"))
+                    question_fields['answer_text'] = question_answer['text'].encode("utf-8")
                     question_fields['answer_status'] = question_answer['status']
                     question_fields['answer_date_created'] = question_answer['date_created']
 
