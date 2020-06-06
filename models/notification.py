a39b77512f572993e8357e324fe667290fe2a7ae
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2020-06-06 19:03:11 +0200

    Added better variation handlings when importing orders

--- models/notification.py
+++ models/notification.py
@@ -111,7 +111,7 @@ class MercadolibreNotification(models.Model):
                                     noti = self.create(vals)
                                     _logger.info("Created new QUESTION notification")
                                     #_logger.info(noti)
-                                    _logger.info(n)
+                                    #_logger.info(n)
                                     questions = meli.get(""+str(n["resource"]), {'access_token':meli.access_token} )
                                     qjson =  questions.json()
                                     if ("item_id" in qjson):
@@ -126,16 +126,16 @@ class MercadolibreNotification(models.Model):
                                     noti = self.create(vals)
                                     _logger.info("Created new ORDER notification.")
                                     #_logger.info(noti)
-                                    _logger.info(n)
+                                    #_logger.info(n)
 
-                            if (n["topic"]=="items"):
+                            if (1==2 and n["topic"]=="items"):
                                 nn = self.search([('notification_id','=',n["_id"])])
                                 if (len(nn)==0):
                                     vals = self._prepare_values(values=n)
                                     noti = self.create(vals)
                                     _logger.info("Created new ITEM notification.")
                                     #_logger.info(noti)
-                                    _logger.info(n)
+                                    #_logger.info(n)
 
                             if (n["topic"] in ["payments"]):
                                 nn = self.search([('notification_id','=',n["_id"])])
@@ -144,7 +144,7 @@ class MercadolibreNotification(models.Model):
                                     noti = self.create(vals)
                                     _logger.info("Created new PAYMENT notification.")
                                     #_logger.info(noti)
-                                    _logger.info(n)
+                                    #_logger.info(n)
 
                     except:
                         _logger.error("Error creating notification.")
