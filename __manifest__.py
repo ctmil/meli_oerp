c052fa61484e762411cec90dbfab4e5ae98126af
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2020-06-10 15:19:30 +0200

    Speed enhancements with notifications, indexes

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': 'MercadoLibre Publisher',
-    'version': '13.0.17.0',
+    'version': '13.0.18.0',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
