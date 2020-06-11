c15048520911d29d53ec116c7db26bc7955148d0
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2020-06-11 16:17:44 +0200

    Updated with predictors

--- models/product.py
+++ models/product.py
@@ -2397,48 +2397,8 @@ class product_product(models.Model):
     def get_title_for_meli(self):
         return self.name
 
-    def get_title_for_category_predictor(self):
-        return self.name
-
-    def get_price_for_category_predictor(self):
-        pricelist = self._get_pricelist_for_meli()
-        return int(self.with_context(pricelist=pricelist.id).price)
-
     def action_category_predictor(self):
-        self.ensure_one()
-        warning_model = self.env['warning']
-        meli_categ, rjson = self._get_meli_category_from_predictor()
-        if meli_categ:
-            self.meli_category = meli_categ.id
-            return warning_model.info( title='MELI WARNING', message="CATEGORY PREDICTOR", message_html="Categoria sugerida: %s" % meli_categ.name)
-        return warning_model.info( title='MELI WARNING', message="CATEGORY PREDICTOR", message_html=rjson)
-
-    def _get_meli_category_from_predictor(self):
-        self.ensure_one()
-        meli_util_model = self.env['meli.util']
-        meli = meli_util_model.get_new_instance()
-        vals = [{
-            'title': self.get_title_for_category_predictor(),
-            'price': self.get_price_for_category_predictor(),
-        }]
-        response = meli.post("/sites/MLM/category_predictor/predict", vals)
-        rjson = response.json()
-        meli_categ = False
-        if rjson and isinstance(rjson, list):
-            if "id" in rjson[0]:
-                meli_categ = self.env['mercadolibre.category'].import_category(rjson[0]['id'])
-        return meli_categ, rjson
-
-    def _get_pricelist_for_meli(self):
-        pricelist = self.env.user.company_id.mercadolibre_pricelist
-        if not pricelist:
-            pricelist = self.env['product.pricelist'].search([
-                ('currency_id','=',self.env.user.company_id.mercadolibre_currency),
-                ('website_id','!=',False),
-            ], limit=1)
-        if not pricelist:
-            pricelist = self.env['product.pricelist'].search([], limit=1)
-        return pricelist
+        return self.product_tmpl_id.action_category_predictor()
 
     #typical values
     meli_title = fields.Char(string='Nombre del producto en Mercado Libre',size=256)
