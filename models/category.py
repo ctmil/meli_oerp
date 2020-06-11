d3855eb412f8fc7bae51cd057b9e2853d9ced709
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2020-06-12 00:58:30 +0200

    Upgrade messages

--- models/category.py
+++ models/category.py
@@ -102,7 +102,7 @@ class mercadolibre_category(models.Model):
                 if (len(rjson_cat["path_from_root"])>=2):
                     fid = int(len(rjson_cat["path_from_root"])-2)
                     #_logger.info(fid)
-                    #_logger.info(rjson_cat["path_from_root"][fid]["id"])
+                    _logger.info(rjson_cat["path_from_root"][fid]["id"])
                     category.meli_father_category_id = rjson_cat["path_from_root"][fid]["id"]
 
 
