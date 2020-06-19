51184994559d918a4223f0c5ad647e8d9fbd60b5
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2020-06-19 12:37:44 +0200

    New fields for tax fixes and variants preservation

--- models/company.py
+++ models/company.py
@@ -292,7 +292,9 @@ class res_company(models.Model):
     mercadolibre_overwrite_template = fields.Boolean(string='Overwrite product template',help='Sobreescribir siempre Nombre y Descripción de la plantilla.')
     mercadolibre_overwrite_variant = fields.Boolean(string='Overwrite product variant',help='Sobreescribir siempre Nombre y Descripción de la variante.')
 
-
+    mercadolibre_create_product_from_order = fields.Boolean(string='Importar productos inexistentes',help='Importar productos desde la orden si no se encuentran en la base.')
+    mercadolibre_update_existings_variants = fields.Boolean(string='Actualiza/agrega variantes',help='Permite agregar y actualizar variantes de un producto existente (No recomendable cuando se está ya en modo Odoo a ML, solo usar cuando se importa por primera vez de ML a Odoo, para no romper el stock)')
+    mercadolibre_tax_included = fields.Boolean(string='Tax Included',help='Tax Included, discount tax when importing products, also, use price_unit with or without taxes when importing order lines.')
 
     def	meli_logout(self):
         _logger.info('company.meli_logout() ')
@@ -692,5 +694,10 @@ class res_company(models.Model):
         #_logger.info("meli_notifications")
         notifications = self.env['mercadolibre.notification']
         return notifications.fetch_lasts()
+
+    def meli_set_automatic_tax_included(self):
+        #create a product with a price of 100, check if tax are created
+        #create an order with this product and check final amount in line.
+
 
 res_company()
