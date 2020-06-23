8f2a8f48ea40c4f2d89402a0e5e5a3b10b7a57e2
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2020-06-23 18:39:02 +0200

    Upgrade to 13.0.20.4

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': 'MercadoLibre Publisher',
-    'version': '13.0.20.3',
+    'version': '13.0.20.4',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
