c0af06db74c23e565a3bc09611131161fb73e43d
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2020-07-07 13:57:03 +0200

    Fix product duplication with new flag Incluir nuevos

--- models/company.py
+++ models/company.py
@@ -628,7 +628,10 @@ class res_company(models.Model):
                     updating = post_update and obj.meli_id and (obj.meli_status=='active')
                     creating = post_new and ( not obj.meli_id or ( obj.meli_id and obj.meli_id == '') )
                     if ( updating or creating):
-                        res = obj.product_post()
+                        if (updating):
+                            res = obj.product_post()
+                        if (creating and obj.product_tmpl_id.meli_pub):
+                            res = obj.product_tmpl_id.product_template_post()
 
                         #we have a message
                         if 'res_id' in res:
