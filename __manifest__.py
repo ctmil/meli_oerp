06ad617532061a3056ad50e0b1f3ee10991474d9
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2020-07-21 10:52:35 +0200

    Upgrade to 0.20.21 optimizing meli_ids

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': 'MercadoLibre Publisher',
-    'version': '13.0.20.20',
+    'version': '13.0.20.21',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
