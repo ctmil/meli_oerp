f71799fe348ca68103486799aa633dc3c924e3fd
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2020-07-22 21:57:25 +0200

    Orders with several variations of the same item, flag for creating products order's

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': 'MercadoLibre Publisher',
-    'version': '12.0.20.21',
+    'version': '12.0.20.23',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
