67691cc3cbf513a6043e8085cef40566952865f8
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2020-07-31 17:02:13 +0200

    Overwrite description

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': 'MercadoLibre Publisher',
-    'version': '13.0.20.26',
+    'version': '13.0.20.27',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
