385bd5ee65e119f9fc89c1b00b14f719d127e930
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2020-08-03 22:11:58 +0200

    Adding seller user and override method for title and description separatly

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': 'MercadoLibre Publisher',
-    'version': '12.0.20.26',
+    'version': '12.0.20.28',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
