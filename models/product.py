a5b737069589f91d6bf533d648b843ca4d95c4bc
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2020-08-06 01:05:04 +0200

    Test multi-image sync

--- models/product.py
+++ models/product.py
@@ -679,6 +679,7 @@ class product_product(models.Model):
                             _logger.info("Image:"+str(len(imagebin))+" vs URLImage:"+str(meli_imagen_bytes)+" diff:"+str(bin_diff) )
                             bin_updating = (abs(bin_diff)>0)
 
+                    _logger.info(pimage)
                     if (pimage==False or (pimage and len(pimage)==0)):
                         _logger.info("Creating new image")
                         bin_updating = True
