d307af55a4e92d183b51f51d93fa3deb8cb8c938
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2020-08-07 12:48:42 +0200

    Starting 14.0.20.29 in master

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': 'MercadoLibre Publisher',
-    'version': '13.0.20.29',
+    'version': '14.0.20.29',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
