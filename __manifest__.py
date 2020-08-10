7d8a424f6083328974c99c1bedbe4c8a80cfe1cf
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2020-08-10 18:37:47 +0200

    Upgrade 12.0.20.30, seller user in pack orders ok, bis

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': 'MercadoLibre Publisher',
-    'version': '12.0.20.29',
+    'version': '12.0.20.30',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
