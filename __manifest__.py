2164b7b6ca77637e8486e9dfdde3c707b755d2b7
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2020-10-08 15:20:03 +0200

    Upgrade update stock for Odoo 13, upgrade to 13.0.20.44

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'MercadoLibre Publisher',
-    'version': '13.0.20.43',
+    'version': '13.0.20.44',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
