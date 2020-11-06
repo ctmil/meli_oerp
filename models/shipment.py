be0bc280a9d8a39e53db1b301eeac1ed2e51711e
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2020-11-06 22:43:41 +0100

    upgrade 11.0.20.54 e

--- models/shipment.py
+++ models/shipment.py
@@ -323,6 +323,8 @@ class mercadolibre_shipment(models.Model):
 
 	pack_order = fields.Boolean(string="Carrito de compra")
 
+    _sql_constraints = [('unique_shipping_id','unique(shipping_id)','Meli Shipping id already exists!')]
+    
 	def create_shipment( self ):
 		return {}
 
@@ -693,8 +695,6 @@ class mercadolibre_shipment(models.Model):
 
 
 		return {}
-
-    _sql_constraints = [ ('unique_shipping_id', 'unique(shipping_id)', 'Mei Shipping id already exists!')]
 
 mercadolibre_shipment()
 
