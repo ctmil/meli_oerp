7c8b4a089bf1b8d02543d6fb032b051f49ac58d8
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2020-11-09 16:34:59 +0100

    Upgrade to 13.0.20.55, catalog listing, orders, shipmentes; buyers unique indexes

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'MercadoLibre Publisher',
-    'version': '13.0.20.54',
+    'version': '13.0.20.55',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
--- models/orders.py
+++ models/orders.py
@@ -1079,6 +1079,10 @@ class mercadolibre_buyers(models.Model):
     billing_info_doc_type = fields.Char( string='Billing Info Doc Type')
     billing_info_doc_number = fields.Char( string='Billing Info Doc Number')
 
+    _sql_constraints = [
+        ('unique_buyer_id', 'unique(buyer_id)', 'Mei Buyer id already exists!')
+    ]
+
 mercadolibre_buyers()
 
 class res_partner(models.Model):
@@ -1087,6 +1091,9 @@ class res_partner(models.Model):
     meli_buyer_id = fields.Char('Meli Buyer Id')
     meli_buyer = fields.Many2one('mercadolibre.buyers',string='Buyer')
 
+    _sql_constraints = [
+        ('unique_partner_meli_buyer_id', 'unique(meli_buyer_id)', 'Mei Partner Buyer id already exists!')
+    ]
 
 res_partner()
 
