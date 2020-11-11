db55806e39b454bc10c34c80fb12c7d34b967dca
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2020-11-11 23:40:26 +0100

    Upgrade test fix attributes and values

--- models/product_attribute.py
+++ models/product_attribute.py
@@ -11,3 +11,9 @@ class ProductAttribute(models.Model):
     meli_id = fields.Char(u'Id Attribute ML')
     meli_default_id_attribute = fields.Many2one('mercadolibre.category.attribute',string="ML Attribute default")
     meli_id_attributes = fields.Many2many('mercadolibre.category.attribute',string="ML Attributes")
+
+
+class ProductAttributeValue(models.Model):
+    _inherit = 'product.attribute.value'
+
+    meli_id = fields.Char(u'Id Attribute Value ML')
