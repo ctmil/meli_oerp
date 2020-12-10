3b79c58105d3dcd6b0f5c23e3d30dfb74c117889
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2020-12-10 18:14:42 +0100

    Upgrade 1X.Y.20.75

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'MercadoLibre Publisher',
-    'version': '13.2.20.74',
+    'version': '13.2.20.75',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
--- models/product.py
+++ models/product.py
@@ -2840,7 +2840,7 @@ class product_product(models.Model):
                 else:
                     product_uom_id = product_uom_id.id
 
-                stock_inventory_fields = _get_inventory_fields(product, wh)
+                stock_inventory_fields = get_inventory_fields(product, wh)
 
                 #_logger.info("stock_inventory_fields:")
                 #_logger.info(stock_inventory_fields)
