63bf4d491df0856f6abb42f572adf57d5abf9aa8
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2020-12-11 20:30:41 +0100

    Upgrade 1X.Y.20.78 wizard with forced import for variants

--- models/product_post.py
+++ models/product_post.py
@@ -225,3 +225,71 @@ class product_post(models.TransientModel):
         return res
 
 product_post()
+
+
+class product_product_update(models.TransientModel):
+    _name = "mercadolibre.product.product.update"
+    _description = "Wizard de Product Product Update en MercadoLibre"
+
+    force_meli_pub = fields.Boolean(string="Forzar importación",help="Forzar importación de todos los seleccionados",default=False)
+    #type = fields.Selection([('post','Alta'),('put','Editado'),('delete','Borrado')], string='Tipo de operación' );
+    #posting_date = fields.Date('Fecha del posting');
+	    #'company_id': fields.many2one('res.company',string='Company'),
+	    #'mercadolibre_state': fields.related( 'res.company', 'mercadolibre_state', string="State" )
+
+
+    def pretty_json( self, data ):
+        return json.dumps( data, sort_keys=False, indent=4 )
+
+    def product_product_update(self, context=None):
+        context = context or self.env.context
+        company = self.env.user.company_id
+        product_ids = []
+        if ('active_ids' in context):
+            product_ids = context['active_ids']
+        product_obj = self.env['product.template']
+
+        #user_obj = self.pool.get('res.users').browse(cr, uid, uid)
+        #user_obj.company_id.meli_login()
+        #company = user_obj.company_id
+        warningobj = self.env['warning']
+
+        #company = self.pool.get('res.company').browse(cr,uid,1)
+
+        REDIRECT_URI = company.mercadolibre_redirect_uri
+        CLIENT_ID = company.mercadolibre_client_id
+        CLIENT_SECRET = company.mercadolibre_secret_key
+        ACCESS_TOKEN = company.mercadolibre_access_token
+        REFRESH_TOKEN = company.mercadolibre_refresh_token
+
+
+        meli = Meli(client_id=CLIENT_ID,client_secret=CLIENT_SECRET, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
+
+        if ACCESS_TOKEN=='' or ACCESS_TOKEN==False:
+            meli = Meli(client_id=CLIENT_ID,client_secret=CLIENT_SECRET)
+            url_login_meli = meli.auth_url(redirect_URI=REDIRECT_URI)
+            return {
+	            "type": "ir.actions.act_url",
+	            "url": url_login_meli,
+	            "target": "new",
+            }
+        res = {}
+        for product_id in product_ids:
+            product = product_obj.browse(product_id)
+            if (product):
+                if self.force_meli_pub and not product.meli_pub:
+                    product.meli_pub = True
+                    #for variant in product.product_variant_ids:
+                    #    variant.meli_pub = True
+                    product.product_tmpl_id.meli_pub = True
+                    for variant in product.product_tmpl_id.product_variant_ids:
+                        variant.meli_pub = True
+                if (product.meli_pub):
+                    res = product.product_template_update()
+
+            if 'name' in res:
+                return res
+
+        return res
+
+product_product_update()
