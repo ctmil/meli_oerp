9b2300c0509ebb0ca58ab787ac357c6b55d58755
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2020-12-22 22:13:52 +0100

    One step update, delivery price null not invoiced

--- models/orders.py
+++ models/orders.py
@@ -90,6 +90,24 @@ class sale_order(models.Model):
     meli_shipment = fields.Many2one('mercadolibre.shipment',string='Meli Shipment Obj')
     meli_shipment_logistic_type = fields.Char(string="Logistic Type",index=True)
 
+    def action_confirm(self):
+        _logger.info("meli order action done")
+        res = super(sale_order,self).action_confirm()
+        try:
+            company = self.env.user.company_id
+            for order in self:
+                for line in order.order_line:
+                    _logger.info(line)
+                    _logger.info(line.is_delivery)
+                    _logger.info(line.price_unit)
+                    if line.is_delivery and line.price_unit<=0.0:
+                        _logger.info(line)
+                        line.write({ "qty_to_invoice": 0.0 })
+                        _logger.info(line.qty_to_invoice)
+        except:
+            pass;
+        return res
+
     def action_done(self):
         _logger.info("meli order action done")
         res = super(sale_order,self).action_done()
