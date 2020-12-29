103855fd2aa475a3432251d501ebeb5bdb348249
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2020-12-29 17:14:29 +0100

    Upgrade to 1X.Y.20.88 meli_login

--- models/company.py
+++ models/company.py
@@ -294,7 +294,7 @@ class res_company(models.Model):
         meli_util_model = self.env['meli.util']
         meli = meli_util_model.get_new_instance()
         _logger.info(meli)
-        url_login_meli = meli.auth_url()
+        url_login_meli = meli.auth_url(redirect_URI=company.redirect_uri)
         _logger.info( "OK company.meli_login() called: url is ", url_login_meli )
 
         return {
