e3e07ff5386355bce844289b65ce8aae1b35eb14
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2020-12-30 11:43:01 +0100

    Upgrade to 1X.Y.20.91 sdk3 RC

--- models/meli_util.py
+++ models/meli_util.py
@@ -76,7 +76,7 @@ class MeliApi( meli.RestClientApi ):
                 "error": "%s" % e,
                 "status": e.status,
                 "cause": e.reason,
-                "message": e.body
+                "message": json.loads(e.body)["message"]
             }
         return self
 
