881693ea18f179551599e7729a880f28760bf8a8
Alejandro Olano <alejocode@gmail.com>
2021-01-05 08:45:56 -0500

    [FIX] Res Currency Rate

--- models/currency.py
+++ models/currency.py
@@ -1,31 +1,31 @@
-
 from odoo import models, fields, api
 from odoo.tools.translate import _
 import math
 
+
 class res_currency_rate(models.Model):
     _inherit = 'res.currency.rate'
 
-    @api.one
-    @api.onchange('rate') # if these fields are changed, call method
+    @api.multi
+    @api.onchange('rate')  # if these fields are changed, call method
     def check_change_rate(self):
+        self.ensure_one()
         self.update_prices()
 
     @api.model
     def update_prices(self):
         #import pdb;pdb.set_trace();
-        products = self.env['product.product'].search([])
+        products = self.env['product.product'].search([('meli_pub', '=', True)
+                                                       ])
 
         pricelists = self.env['product.pricelist'].search([])
         #pricelist = pricelists[0]
         for pricelist in pricelists:
             if self.currency_id.name == pricelist.currency_id.name:
                 for product in products:
-                    if self.rate>0 and not product.meli_price_fixed:
-                        new_price = math.ceil( product.lst_price / self.rate )
-                        vals = {
-                            'meli_price': str(new_price)
-                        }
+                    if self.rate > 0 and not product.meli_price_fixed:
+                        new_price = math.ceil(product.lst_price / self.rate)
+                        vals = {'meli_price': str(new_price)}
                         product.write(vals)
                         product.product_post()
 
