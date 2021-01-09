844ebcd48766adf5688b57acc2a83fe153799c28
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-01-09 01:37:11 +0100

    Upgrade 1X.Y.21.06, connection protocol upgraded

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'MercadoLibre Publisher',
-    'version': '12.0.21.05',
+    'version': '12.0.21.06',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
