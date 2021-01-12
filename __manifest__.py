3636af2cbc35ac3ef53ef194b6f3dee4e5ed47d4
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-01-12 20:56:29 +0100

    Upgrade 1X.Y.21.08 fix invalid token body message

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'MercadoLibre Publisher',
-    'version': '12.0.21.07',
+    'version': '12.0.21.08',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
