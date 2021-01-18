f59eb87bddf5b441df36f2dac4c27146beed2f4f
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-01-18 16:20:39 +0100

    Upgrade 1X.Y.21.13

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'MercadoLibre Publisher',
-    'version': '14.0.21.12',
+    'version': '14.0.21.13',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
