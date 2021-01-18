a4ea7f9971332df5eea5ff474ff0fba777a252e2
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-01-18 20:11:16 +0100

    Upgrade 1X.Y.21.15, authorization updated for all latam countries

--- models/meli_util.py
+++ models/meli_util.py
@@ -228,7 +228,7 @@ class MeliUtil(models.AbstractModel):
         api_rest_client.access_token = company.mercadolibre_access_token or ''
         api_rest_client.refresh_token = company.mercadolibre_refresh_token
         api_rest_client.redirect_uri = company.mercadolibre_redirect_uri
-        api_rest_client.AUTH_URL = company.get_ML_AUTH_URL(meli=meli)
+        api_rest_client.AUTH_URL = company.get_ML_AUTH_URL(meli=api_rest_client)
         api_auth_client = meli.OAuth20Api(api_client)
         grant_type = 'authorization_code' # or 'refresh_token' if you need get one new token
         last_token = api_rest_client.access_token
