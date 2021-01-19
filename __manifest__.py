57761ca2f49cf604a18ba38d1c7c0e4133ca2233
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-01-19 20:26:48 +0100

    Upgrade 1X.Y.21.17, enhancing notification logs for massive audit and stock nullifications

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'MercadoLibre Publisher',
-    'version': '13.0.21.16',
+    'version': '13.0.21.17',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
