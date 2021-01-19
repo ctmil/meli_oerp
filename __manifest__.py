ef2a17f5c39478b7ce55fe9ebd8f745c1a3e9631
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-01-19 21:39:55 +0100

    Upgrade 1X.Y.21.18, notifications are back to stay, ready for internal messaging, and batch iterations

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'MercadoLibre Publisher',
-    'version': '12.0.21.17',
+    'version': '12.0.21.18',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
