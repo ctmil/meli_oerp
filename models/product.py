31fa0296ed288cb25104b3185b46480455bf3228
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-03-05 16:06:36 +0100

    Upgrade 1X.Y.21.25, posting price optimized for variants

--- models/product.py
+++ models/product.py
@@ -248,10 +248,10 @@ class product_template(models.Model):
                     variant.product_meli_delete()
         return {}
 
-    @api.onchange('meli_pub') # if these fields are changed, call method
-    def change_meli_pub(self):
-        for product in self:
-            _logger.info("onchange meli_pub:"+str(product))
+    #@api.onchange('meli_pub') # if these fields are changed, call method
+    #def change_meli_pub(self):
+    #    for product in self:
+    #        _logger.info("onchange meli_pub:"+str(product))
             #product = self._origin
             #product = self
             #for variant in product.product_variant_ids:
