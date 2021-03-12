d988e4685452e56ba2350e97632ecc320dca91d6
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-03-12 18:07:07 +0100

    Fix name and summary

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,8 @@
 ##############################################################################
 
 {
-    'name': 'MercadoLibre Publisher',
+    'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
+    'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'version': '11.0.21.28',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
@@ -28,15 +29,26 @@
         'data/cron_jobs.xml',
         'data/error_template_data.xml',
         'data/parameters_data.xml',
-	'report/report_shipment_view.xml',
+	    'report/report_shipment_view.xml',
         'report/report_invoice_shipment_view.xml',
-	'views/shipment_view.xml',
-	'views/notifications_view.xml'
+    	'views/shipment_view.xml',
+    	'views/notifications_view.xml'
     ],
+    'price': '350.00',
+    'currency': 'USD',
     "external_dependencies": {"python": ['pdf2image','meli']},
+    'images': [ 'static/description/main_screenshot.png',
+                'static/description/meli_oerp_screenshot.png',
+                'static/description/meli_oerp_configuration_1.png',
+                'static/description/meli_oerp_configuration_2.png',
+                'static/description/meli_oerp_configuration_3.png',
+                'static/description/meli_oerp_configuration_4.png',
+                'static/description/moldeo_interactive_logo.png',
+                'static/description/odoo_to_meli.png'],
     'demo_xml': [],
     'active': False,
     'installable': True,
     'application': True,
+    'license': 'GPL-3'
 
 }
