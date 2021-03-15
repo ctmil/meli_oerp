b53828ba2981ae6eb8a87553ebc0ef48fe6ba923
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-03-15 14:30:33 +0100

    Upgrade 1X.Y.21.30, more functional programming enhancements for extension compatibilities

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '12.0.21.29',
+    'version': '12.0.21.30',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
