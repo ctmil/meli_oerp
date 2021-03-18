a66d24d84ba84d0131c251d780416bc9dc854687
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-03-19 00:19:06 +0100

    Upgrade 1X.Y.21.33 meli updated

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '11.0.21.32',
+    'version': '11.0.21.33',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
