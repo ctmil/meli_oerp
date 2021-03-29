17f6d5176954280986ba34761e569eec22e1db5e
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-03-29 23:03:18 +0200

    Upgrade 1X.Y.21.34, multi account compatibility

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '14.0.21.33',
+    'version': '14.0.21.34',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
