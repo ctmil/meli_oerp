309588a31c04b13f7a4a90b5c422194f51d6cc42
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-04-15 15:20:55 +0200

    Upgrade 1X.Y.21.48 upgraded for l10n_latam types odoo >= 13.0

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '14.0.21.46',
+    'version': '14.0.21.48',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
