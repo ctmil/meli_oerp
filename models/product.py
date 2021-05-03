515814c9fdfb01767cc7c1d7ea82753ee5e68955
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-05-03 19:13:07 +0200

    Upgrade 1X.Y.21.56, upgrading adding shipping methods/modes, first try

--- models/product.py
+++ models/product.py
@@ -388,6 +388,10 @@ class product_template(models.Model):
     meli_catalog_item_relations = fields.Char(string='Catalog Item Relations', size=256)
     meli_catalog_automatic_relist = fields.Boolean(string='Catalog Auto Relist', size=256)
 
+    meli_shipping_mode = fields.Char(string="Shipping Mode",help="Shipping modes (por usuario): custom, not_specified, me2. https://api.mercadolibre.com/users/USERID/shipping_preferences",index=True)
+    meli_shipping_method = fields.Char(string="Shipping Method",help="Shipping methods: https://api.mercadolibre.com/sites/SITEID/shipping_methods",index=True)
+
+
 product_template()
 
 class product_product(models.Model):
@@ -3032,6 +3036,9 @@ class product_product(models.Model):
     meli_shipping_logistic_type = fields.Char(string="Logistic Type",index=True)
 
     meli_inventory_id = fields.Char(string="Inventory Id",index=True)
+
+    meli_shipping_mode = fields.Char(string="Shipping Mode",help="Shipping modes (por usuario): custom, not_specified, me2. https://api.mercadolibre.com/users/USERID/shipping_preferences",index=True)
+    meli_shipping_method = fields.Char(string="Shipping Method",help="Shipping methods: https://api.mercadolibre.com/sites/SITEID/shipping_methods",index=True)
 
     _defaults = {
         'meli_imagen_logo': 'None',
