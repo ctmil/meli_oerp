8d1e92381b38344215c0c0f54d5032e5cdef2341
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-05-06 11:26:55 +0200

    Upgrade 1X.Y.21.59, config payment term added

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '12.0.21.58',
+    'version': '12.0.21.59',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
