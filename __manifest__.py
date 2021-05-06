35da463a6d39b8f8be7435e320905cc1c49a978a
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-05-06 21:04:04 +0200

    Upgrade 21.64 shipment_print_report

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '14.0.21.63',
+    'version': '14.0.21.64',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
--- models/shipment.py
+++ models/shipment.py
@@ -125,7 +125,8 @@ class mercadolibre_shipment_print(models.TransientModel):
         reporte = ""
         full_url_link_pdf = {}
         shipment_obj = self.env['mercadolibre.shipment']
-        
+        warningobj = self.env['warning']
+
         for shipid in shipment_ids:
             shipment = shipment_obj.browse(shipid)
             ship_report = shipment.shipment_print( meli=meli, config=config, include_ready_to_print=include_ready_to_print )
