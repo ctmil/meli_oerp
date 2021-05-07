83bd769635a10de3e21abab2aa3af6188edc552e
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-05-07 19:11:15 +0200

    Upgrade 1X.Y.21.66, upgrade shipment cost based on base_cost and self_service

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '13.0.21.65',
+    'version': '13.0.21.66',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
