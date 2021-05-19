3cd279906f5c8b004c40ed163be1d350c5c4d484
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-05-19 23:06:17 +0200

    Upgrade 1X.Y.21.73, removed constraints for product.product

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '12.0.21.72',
+    'version': '12.0.21.73',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
