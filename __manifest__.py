2c9292732b140e0c847dd90782dd4d0a89ef788e
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-07-13 16:03:03 +0200

    Upgrade 1X.Y.21.98, packed orders total amount calculated using sum of each order total amount, best precision for invoice conciliation , and total config parameter value based on transaction amount, not paid amount

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '12.0.21.96',
+    'version': '12.0.21.98',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
