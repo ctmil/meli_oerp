a495eb8d411dfea4fe0703d228ad9747fda04ae2
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-08-02 16:39:58 +0200

    Upgrade 1X.Y.21.107, questions update, crons separation

--- models/product_attribute.py
+++ models/product_attribute.py
@@ -3,6 +3,8 @@
 from odoo import models, api, fields, tools
 import odoo.addons.decimal_precision as dp
 from odoo.tools.translate import _
+from . import versions
+from .versions import *
 
 class ProductAttribute(models.Model):
 
@@ -11,3 +13,15 @@ class ProductAttribute(models.Model):
     meli_id = fields.Char(u'Id Attribute ML')
     meli_default_id_attribute = fields.Many2one('mercadolibre.category.attribute',string="ML Attribute default")
     meli_id_attributes = fields.Many2many('mercadolibre.category.attribute',string="ML Attributes")
+
+    def meli_default_create_variant( self, meli_attribute=None ):
+        
+        create_variant = default_create_variant
+        
+        if meli_attribute and "variation_attribute" in meli_attribute:
+            if not meli_attribute["variation_attribute"]:
+                create_variant = default_no_create_variant
+                    
+        
+        return create_variant
+        
