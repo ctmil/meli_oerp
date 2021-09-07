5184523abba8ad79a5d746ee10a8a68d50124e40
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-09-07 19:28:37 +0200

    Upgrade 13.0.imagesvar

--- models/product.py
+++ models/product.py
@@ -2560,10 +2560,12 @@ class product_product(models.Model):
                                     var_multi_images_ids = var_product.product_meli_upload_multi_images(meli=meli,config=config)
 
                                     var_pics.append(var_product.meli_imagen_id)
+                                    var_pics_full.append(var_product.meli_imagen_id)
                                     if (var_multi_images_ids):
                                         for pic in var_multi_images_ids:
                                             var_pics.append(pic['id'])
-                                    var_pics_full+= var_pics
+                                            var_pics_full.append(pic['id'])
+
                                     vars_updated+= var_product
 
                             #TODO: add SKU
