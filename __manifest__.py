8100909be822e25ffa85d95a9b258f86f8311ff9
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-09-07 19:39:47 +0200

    Upgraded 118 with image vars

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '12.0.21.117',
+    'version': '12.0.21.118',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
