9f4abf2f09cc3edc64e0430d6a62db0576f02993
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-09-14 16:11:37 +0200

    Upgrade 120

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '11.0.21.119',
+    'version': '11.0.21.120',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
