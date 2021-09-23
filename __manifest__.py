c999d89a524be31bacf1965ba89ac2c55b5bd4f7
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-09-23 12:27:38 +0200

    Upgrade 123, block stock update field for products

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '13.0.21.122',
+    'version': '13.0.21.123',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
