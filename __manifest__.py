945778d854bcd1881916a95971e5f9e4867ad9d7
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-11-08 17:02:58 +0100

    Upgrade 143, optimizing and fixing realtime prices and stock moves and post stock for every versions from 15.0 to 11.0

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '14.0.21.141',
+    'version': '14.0.21.143',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
