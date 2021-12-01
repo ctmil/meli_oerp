b7c55ae42869c91afdb760bb1c62e1a85265144a
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-12-01 15:47:07 +0100

    Upgrade 151, added edit links and status search and logistic group

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '12.0.21.150',
+    'version': '12.0.21.151',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
