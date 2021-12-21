a5c138df03e5b06abfe12f21de3a5d1f2e5a06b0
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-12-21 21:02:07 +0100

    Upgrade 155 sale terms MANUFACTURING TIME

--- models/product.py
+++ models/product.py
@@ -2620,6 +2620,9 @@ class product_product(models.Model):
 
         #if (product_tmpl.meli_brand==False or len(product_tmpl.meli_brand)==0):
         #    product_tmpl.meli_brand = ("product_brand_id" in product_tmpl._fields and product_tmpl.product_brand_id and product_tmpl.product_brand_id.id and product_tmpl.product_brand_id.display_name )
+        if product_tmpl.meli_brand == "wk.product.brand()" or product.meli_brand == "wk.product.brand()":
+            product_tmpl.meli_brand = ""
+            product.meli_brand = ""    
         if product.meli_brand==False or len(product.meli_brand)==0:
             product.meli_brand = product_tmpl.meli_brand
         if product.meli_model==False or len(product.meli_model)==0:
