ea1eb147c2077701efd2a9c696c39eb8e99e9c8e
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-12-21 20:07:27 +0100

    Upgrade 155 sale terms

--- models/product.py
+++ models/product.py
@@ -2967,7 +2967,7 @@ class product_product(models.Model):
                         del body['available_quantity']
                         _logger.info("update post:"+str(body))
                         resbody = product.meli_id and meli.put("/items/"+product.meli_id, body, {'access_token':meli.access_token})
-                        #_logger.debug(resbody.json())
+                        _logger.info(str(resbody and resbody.json()))
                          #responsevar = meli.put("/items/"+product.meli_id, {"initial_quantity": product.meli_available_quantity, "available_quantity": product.meli_available_quantity }, {'access_token':meli.access_token})
                          #_logger.debug(responsevar)
                          #_logger.debug(responsevar.json())
@@ -3024,6 +3024,7 @@ class product_product(models.Model):
                 del body['available_quantity']
                 _logger.info("update post:"+str(body))
                 resbody = product.meli_id and meli.put("/items/"+product.meli_id, body, {'access_token':meli.access_token})
+                _logger.info(str(resbody and resbody.json()))
                 return {}
 
         #check fields
