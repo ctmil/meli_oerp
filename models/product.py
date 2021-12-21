8625f7a34fb57250df48789147d148688adc76dc
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-12-21 21:36:35 +0100

    Upgrade 155 sale terms MANUFACTURING TIME

--- models/product.py
+++ models/product.py
@@ -2661,7 +2661,7 @@ class product_product(models.Model):
                         attributes_ids[attribute["id"]] = attribute["value_name"]
                         attributes.append(attribute)
 
-                    if (at_line_id.attribute_id.meli_default_id_attribute.id and at_line_id.attribute_id.meli_default_id_attribute.variation_attribute==True and and at_line_id.attribute_id.meli_default_id_attribute.hidden==False):
+                    if (at_line_id.attribute_id.meli_default_id_attribute.id and at_line_id.attribute_id.meli_default_id_attribute.variation_attribute==True and at_line_id.attribute_id.meli_default_id_attribute.hidden==False):
                         variations_candidates = True
 
                 elif (len(at_line_id.value_ids)>1):
