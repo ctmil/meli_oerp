0dc8249ba1d90142874cf0efee17766d8cf5a20b
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2021-12-30 16:55:59 +0100

    Upgrading 161, sale terms new method

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '12.0.21.160',
+    'version': '12.0.21.161',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
