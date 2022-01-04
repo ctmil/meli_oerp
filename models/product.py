5dcc05adc8d64140210932013a2caa85991e3916
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2022-01-04 15:55:19 +0100

    Upgrade 22.1, minor fixes, less info logs

--- models/product.py
+++ models/product.py
@@ -2743,16 +2743,7 @@ class product_product(models.Model):
             "condition": product.meli_condition  or '',
             "available_quantity": product.meli_available_quantity  or '0',
             #"warranty": product.meli_warranty or '',
-            "sale_terms":[
-                 {
-                    "id":"WARRANTY_TYPE",
-                    "value_name":"Garantía del vendedor"
-                 },
-                 {
-                    "id":"WARRANTY_TIME",
-                    "value_name": product.meli_warranty
-                 }
-              ],
+            "sale_terms": product._update_sale_terms( meli=meli, productjson=productjson ),
             #"pictures": [ { 'source': product.meli_imagen_logo} ] ,
             "video_id": product.meli_video  or '',
         }
@@ -2808,16 +2799,7 @@ class product_product(models.Model):
                 #"condition": product.meli_condition or '',
                 "available_quantity": product.meli_available_quantity or '0',
                 #"warranty": product.meli_warranty or '',
-                "sale_terms":[
-                     {
-                        "id":"WARRANTY_TYPE",
-                        "value_name":"Garantía del vendedor"
-                     },
-                     {
-                        "id":"WARRANTY_TIME",
-                        "value_name": product.meli_warranty
-                     }
-                  ],
+                "sale_terms": product._update_sale_terms( meli=meli, productjson=productjson ),
 
                 "pictures": [],
                 "video_id": product.meli_video or '',
