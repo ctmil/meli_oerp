8932f4bac9baa883bb169cde6a3bda851d5e4559
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2022-01-04 15:55:19 +0100

    Upgrade 22.1, minor fixes, less info logs

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '12.0.21.161',
+    'version': '12.0.22.1',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
