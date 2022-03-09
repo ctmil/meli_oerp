3e7202af798a6253a75d8bfba21025925800416f
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2022-03-09 19:51:28 +0100

    Upgrade 22.18, adding product.pricelist.item view

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '14.0.22.17',
+    'version': '14.0.22.18',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
