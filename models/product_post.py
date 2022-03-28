abbdb0db9fbdb1b4439e873fabbcd2def53f8902
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2022-03-28 17:47:47 +0200

    Upgrading adding batch import

--- models/product_post.py
+++ models/product_post.py
@@ -290,7 +290,7 @@ class product_template_import(models.TransientModel):
                 imp.report_import = 'report_import' in sync_status and sync_status['report_import']
                 if imp.report_import:
                     imp.report_import_link = 'report_import_link' in sync_status and str(sync_status['report_import_link'])
-                    _logger.info('_calculate_sync_status: ' + str(imp.report_import_link))
+                    _logger.info('_calculate_sync_status: imp.report_import_link > ' + str(imp.report_import_link))
 
 
     actives_to_sync = fields.Char(string="Products actives to sync",compute=_calculate_sync_status)
