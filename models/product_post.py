c101b80fb88802cde4f0bd1f5541e4c820b8737b
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2022-03-31 20:24:14 +0200

    Upgraded

--- models/product_post.py
+++ models/product_post.py
@@ -321,6 +321,8 @@ class product_template_import(models.TransientModel):
 
         company = self.env.user.company_id
         product_obj = self.env['product.product']
+        meli_ids = product_obj.search([('meli_id','!=',False)]).mapped('meli_id')
+        _logger.info("meli_ids:"+str(meli_ids))
 
         meli = self.env['meli.util'].get_new_instance(company)
         if meli.need_login():
