df6cbb23a0403649de298624fa2b40ac7748fd90
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2022-12-16 12:42:15 +0100

    Working commit 22.66 for Odoo 16.0

--- models/warning.py
+++ models/warning.py
@@ -143,7 +143,7 @@ class warning(models.TransientModel):
         """Get the view id
         @return: view id, or False if no view found
         """
-        res = self.env['ir.model.data'].check_object_reference( WARNING_MODULE, 'warning_form')
+        res = get_ref_view( self, WARNING_MODULE, 'warning_form')
         return res and res[1] or False
 
     def _message(self, id, context=None):
