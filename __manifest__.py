6ca03068066eb1679ce085e831acee4b50b3984f
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2023-01-13 12:59:32 +0100

    Upgraded 23.3 fix syntax vat and doc type for non doc type undefined

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '14.0.23.1',
+    'version': '14.0.23.3',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
