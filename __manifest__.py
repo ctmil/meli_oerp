048846af4f72dee0f69f1e4ffa7ec42fa4f63617
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2023-06-15 14:08:32 -0300

    Upgraded 23.39 fixed old model warning

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '13.0.23.38',
+    'version': '13.0.23.39',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
