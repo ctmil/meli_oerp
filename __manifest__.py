7dddf310fef61a9b911f6fbf1128669baf24a16f
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2023-08-09 21:48:25 +0200

    23.49 fix massive import view

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '16.0.23.48', 
+    'version': '16.0.23.49', 
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
