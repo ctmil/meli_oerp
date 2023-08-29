dd6351fa7bb73b14dfb0c55a2058fe8241bc6666
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2023-08-29 21:17:24 +0200

    Upgraded 23.54

--- models/product_image.py
+++ models/product_image.py
@@ -18,7 +18,7 @@ import base64
 from odoo import api, fields, models, tools, _
 from odoo.exceptions import ValidationError
 
-from odoo.addons.web_editor.tools import get_video_embed_code, get_video_thumbnail
+#from odoo.addons.web_editor.tools import get_video_embed_code, get_video_thumbnail
 
 
 from datetime import datetime
@@ -33,7 +33,9 @@ if (not ('replace' in string.__dict__)):
 from . import versions
 from .versions import *
 
-from odoo.addons.web_editor.tools import get_video_embed_code, get_video_thumbnail
+from odoo.addons.website.tools import get_video_embed_code
+
+#from odoo.addons.web_editor.tools import get_video_embed_code, get_video_thumbnail
 
 class ProductImage(models.Model):
 
@@ -85,33 +87,33 @@ class MeliImage(models.Model):
 
     #product_tmpl_id = fields.Many2one('product.template', "Product Template", index=True, ondelete='cascade')
     #product_variant_id = fields.Many2one('product.product', "Product Variant", index=True, ondelete='cascade')
-    video_url = fields.Char('Video URL',
-                            help='URL of a video for showcasing your product.')
-    embed_code = fields.Html(compute="_compute_embed_code", sanitize=False)
+    #video_url = fields.Char('Video URL',
+    #                        help='URL of a video for showcasing your product.')
+    #embed_code = fields.Html(compute="_compute_embed_code", sanitize=False)
 
-    can_image_1024_be_zoomed = fields.Boolean("Can Image 1024 be zoomed", compute='_compute_can_image_1024_be_zoomed', store=True)
+    #can_image_1024_be_zoomed = fields.Boolean("Can Image 1024 be zoomed", compute='_compute_can_image_1024_be_zoomed', store=True)
 
-    @api.depends('image_1920', 'image_1024')
-    def _compute_can_image_1024_be_zoomed(self):
-        for image in self:
-            image.can_image_1024_be_zoomed = image.image_1920 and tools.is_image_size_above(image.image_1920, image.image_1024)
+    #@api.depends('image_1920', 'image_1024')
+    #def _compute_can_image_1024_be_zoomed(self):
+    #    for image in self:
+    #        image.can_image_1024_be_zoomed = image.image_1920 and tools.is_image_size_above(image.image_1920, image.image_1024)
 
-    @api.onchange('video_url')
-    def _onchange_video_url(self):
-        if not self.image_1920:
-            thumbnail = get_video_thumbnail(self.video_url)
-            self.image_1920 = thumbnail and base64.b64encode(thumbnail) or False
+    #@api.onchange('video_url')
+    #def _onchange_video_url(self):
+    #    if not self.image_1920:
+    #        thumbnail = get_video_thumbnail(self.video_url)
+    #        self.image_1920 = thumbnail and base64.b64encode(thumbnail) or False
 
-    @api.depends('video_url')
-    def _compute_embed_code(self):
-        for image in self:
-            image.embed_code = get_video_embed_code(image.video_url) or False
+    #@api.depends('video_url')
+    #def _compute_embed_code(self):
+    #    for image in self:
+    #        image.embed_code = get_video_embed_code(image.video_url) or False
 
-    @api.constrains('video_url')
-    def _check_valid_video_url(self):
-        for image in self:
-            if image.video_url and not image.embed_code:
-                raise ValidationError(_("Provided video URL for '%s' is not valid. Please enter a valid video URL.", image.name))
+    #@api.constrains('video_url')
+    #def _check_valid_video_url(self):
+    #    for image in self:
+    #        if image.video_url and not image.embed_code:
+    #            raise ValidationError(_("Provided video URL for '%s' is not valid. Please enter a valid video URL.", image.name))
 
     meli_imagen_id = fields.Char(string='Imagen Id',index=True)
     meli_imagen_link = fields.Char(string='Imagen Link')
