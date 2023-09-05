5a6f525768b26d04483557928e3ed02800aba068
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2023-09-05 21:09:03 +0200

    Upgraded 23.55

--- models/product.py
+++ models/product.py
@@ -3774,7 +3774,7 @@ class product_product(models.Model):
     #post only fields
     meli_post_required = fields.Boolean(string='Publicable', help='Este producto es publicable en Mercado Libre')
     meli_id = fields.Char(string='ML Id', help='Id del item asignado por Meli', size=256, index=True)
-    meli_description_banner_id = fields.Many2one("mercadolibre.banner",string="Description Banner")
+    #meli_description_banner_id = fields.Many2one("mercadolibre.banner",string="Description Banner")
     meli_buying_mode = fields.Selection(string='Método',help='Método de compra',selection=[("buy_it_now","Compre ahora"),("classified","Clasificado")])
     meli_price_fixed = fields.Boolean(string='Price is fixed')
     meli_available_quantity = fields.Integer(string='Cantidades', help='Cantidad disponible a publicar en ML')
