b98cfab8557a9f5cdf0f8270de7b07c7d7e7beb8
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2023-10-06 16:13:30 +0200

    Upgraded 23.62 view banner images

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '16.0.23.61', 
+    'version': '16.0.23.62', 
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
