a72742b74e65b238a4c8f85fd8db591de0b8504a
Fabricio Costa Alisedo <fabricio.costa@moldeointeractive.com.ar>
2024-02-05 16:53:32 +0100

    Upgraded 24.8, minor fixes, added post barcode option and better chart guide features

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '17.0.23.68',
+    'version': '17.0.24.8',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
