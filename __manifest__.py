815b2f5ff78ebfbf3efad403bb7329f58b515b21
Fabricio Costa Alisedo <fabricio.costa@moldeointeractive.com.ar>
2024-10-29 14:07:13 +0100

    Upgraded 18.0.24.58

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '17.0.24.58',
+    'version': '18.0.24.58',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
