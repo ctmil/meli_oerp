6429deea509384d14e3cb448f9fc7b8aa911427c
Fabricio Costa Alisedo <fabricio.costa@moldeointeractive.com.ar>
2025-05-22 22:16:30 +0200

    Upgraded 25.6 minor fixes added access tokens to category discovery

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '18.0.25.5',
+    'version': '18.0.25.6',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
