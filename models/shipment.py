93ee45599fb23425df6e8721cf8382fd40005d96
Fabricio Costa Alisedo <fabricio.costa@moldeointeractive.com.ar>
2025-07-15 18:52:59 +0200

    up minor fixes ok 25.11 fee details and charges details

--- models/shipment.py
+++ models/shipment.py
@@ -1061,6 +1061,8 @@ class mercadolibre_shipment(models.Model):
                         if (sorder_pack and sorder_pack.meli_update_forbidden):
                             _logger.error("Forbidden to update sale order by meli_oerp" )
                             return {'error': 'Forbidden to update sale order by meli_oerp' }
+
+                        sorder = sorder_pack
                         totales = {}
                         totales['total_amount'] = 0
                         totales['paid_amount'] = 0
@@ -1099,8 +1101,6 @@ class mercadolibre_shipment(models.Model):
                             #TODO: "add parameter for pack_id":
                             #'name': "ML %i" % ( all_orders[0]["pack_id"] ),
                             #'name': "ML %s" % ( str(all_orders[0]["order_id"]) ),
-                            'partner_id': partner_id.id,
-                            'partner_invoice_id': partner_invoice_id and partner_invoice_id.id,
                             'pricelist_id': plistid and plistid.id,
                             #'meli_order_id': '%i' % (order_json["id"]),
                             'meli_order_id': packed_order_ids,
@@ -1121,6 +1121,18 @@ class mercadolibre_shipment(models.Model):
                         })
                         #TODO: agregar un campo para diferencia cada delivery res partner al shipment y orden asociado, crear un binding usando values diferentes... y listo
                         #_logger.info("ship_json[receiver_address]:"+str(ship_json["receiver_address"]) )
+
+
+
+                        if partner_id:
+                            partner_already_set = (sorder and sorder.partner_id and sorder.partner_id.id == partner_id.id)
+                            if not partner_already_set:
+                                meli_order_fields.update({'partner_id': (partner_id and partner_id.id)})
+
+                        if partner_invoice_id:
+                            partner_invoice_already_set = (sorder and sorder.partner_invoice_id and sorder.partner_invoice_id.id == partner_invoice_id.id)
+                            if not partner_invoice_already_set:
+                                meli_order_fields.update({'partner_invoice_id': (partner_invoice_id and partner_invoice_id.id)})
 
 
                         if partner_shipping_id:
