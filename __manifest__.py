3ddadc7d77b6a241dc40df940d95cc704770229d
Fabricio Costa Alisedo <fabricio.costa@moldeointeractive.com.ar>
2025-10-15 21:24:28 +0200

    Upgraded 25.23 improved stock post logic, odoo contacts vs meli buyers multicompany options

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '18.0.25.22',
+    'version': '18.0.25.23',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
