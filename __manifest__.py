46f67723f604e51b4ed5b6538739a6598f7ba0ac
Fabricio Costa Alisedo <fabricio.costa@moldeointeractive.com.ar>
2025-11-25 20:19:37 +0100

    25.36 fix user_product_id for variants, fix 19 product_uom to product_uom_id

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '18.0.25.35',
+    'version': '18.0.25.36',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
