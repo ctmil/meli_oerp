6bb584a6a93645fa6318a5362f322c570f882751
Fabricio Costa Alisedo <fabricio.costa@moldeointeractive.com.ar>
2026-01-07 13:27:50 +0100

    up 25.46 minor clean

--- models/company.py
+++ models/company.py
@@ -323,7 +323,7 @@ class res_company(models.Model):
     mercadolibre_category_import = fields.Char( string='Category to import', help='Category Code to Import, check Recursive Import to import the full tree', size=256)
     mercadolibre_recursive_import = fields.Boolean( string='Recursive import', help='Import all the category tree from Category Code')
 
-    mercadolibre_sending_message_to_customer = fields.Boolean(string='Activate sending message to customer')
+    #mercadolibre_sending_message_to_customer = fields.Boolean(string='Activate sending message to customer')
 
     mercadolibre_cron_refresh = fields.Boolean(string='Keep alive',help='Cron Automatic Token Refresh for keeping ML connection alive.')
     mercadolibre_cron_mail = fields.Many2one(
