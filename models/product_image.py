b71dd6fa93734e7dd5f7a0b173dac8ff662b9652
Fabricio Costa Alisedo <fabricio.costa@moldeointeractive.com.ar>
2026-03-26 17:28:06 -0300

    Up 26.20 stock multi-warehouse seller_address vs seller_warehouse auto check ok!

--- models/product_image.py
+++ models/product_image.py
@@ -47,9 +47,13 @@ class ProductImage(models.Model):
     meli_force_pub = fields.Boolean(string='Publicar en ML y conservar en Odoo',index=True)
     meli_published = fields.Boolean(string='Publicado en ML',index=True)
 
-    _sql_constraints = [
-        ('unique_meli_imagen_id', unique_meli_imagen_id_fields, 'Meli Imagen Id already exists!')
-    ]
+    _unique_meli_imagen_id = versions.UniqueIndex(
+        'product_tmpl_id, product_variant_id, meli_imagen_id',
+        message='Meli Imagen Id already exists!',
+    )
+    _sql_constraints = versions.sql_constraints_if_no_unique_index([
+        ('unique_meli_imagen_id', 'product_tmpl_id, product_variant_id, meli_imagen_id', 'Meli Imagen Id already exists!'),
+    ])
 
     def calculate_hash(self):
         hexhash = ''
