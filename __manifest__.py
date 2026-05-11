c2a02540d9caa7ebade3630c7eeb851ba47c74c5
Fabricio Costa Alisedo <fabricio.costa@moldeointeractive.com.ar>
2026-05-11 17:45:31 -0300

    up 26.28/29 fixes minor col/mex/arg

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '17.0.26.28',
+    'version': '17.0.26.29',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
